db750f1d1eb7f16a7db91901867df46a82d39621
Roberto Alagemovits <robertomovits@gmail.com>
2023-04-29 15:17:23 -0300

    added conversation hisotry and changed  q/a to prompt

--- GPT-Bot.py
+++ GPT-Bot.py
@@ -18,7 +18,9 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-def generate_response(prompt, engine):
+conversation_history = {}
+
+def generate_response(prompt, engine, conversation_id):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {OPENAI_API_KEY}'
@@ -29,22 +31,34 @@ def generate_response(prompt, engine):
         'max_tokens': 1000,
         'n': 1,
         'stop': None,
-        'temperature': 0,
+        'temperature': 0.7,
     }
+
+    if conversation_id in conversation_history:
+        data['choices'] = conversation_history[conversation_id]
 
     gpt_api_url = f'https://api.openai.com/v1/engines/{engine}/completions'
     response = requests.post(gpt_api_url, headers=headers, json=data)
     result = json.loads(response.text)
-    return result['choices'][0]['text'].strip()
+    response_text = result['choices'][0]['text'].strip()
+
+    if conversation_id not in conversation_history:
+        conversation_history[conversation_id] = []
+
+    conversation_history[conversation_id].append(prompt)
+    conversation_history[conversation_id].append(response_text)
+
+    return response_text
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='ask')
-async def ask(ctx, *, question):
-    prompt = f'Q: {question}\nA:'
-    response = generate_response(prompt, 'text-davinci-003')
+async def ask(ctx, *, user_input):
+    conversation_id = f'{ctx.guild.id}-{ctx.author.id}'
+    prompt = f'User: {user_input}\nAI:'
+    response = generate_response(prompt, 'text-davinci-003', conversation_id)
     answer = response.replace(prompt, '').strip()
     await ctx.send(answer)
 
